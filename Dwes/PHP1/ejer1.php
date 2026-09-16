10a38f52028cd462f8bd11fc1f85e14d3f7643bf
Daniihzz <danyynavarro2812@gmail.com>
2026-09-16 09:43:22 +0200

    update 2026-09-16 09:43:22

---
+++ Dwes/PHP1/ejer1.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello world</title>
+</head>
+
+<body>
+    <p>La siguiente linea esta hecha con PHP:</p>
+    <?php
+    echo "<p>hello world!</p>";
+
+    ?>
+    <p>Esta linea tmb</p>
+    <p>
+        <?php
+        echo "Hola mundo";
+        echo "<br>";
+        echo "otra cosa";
+        echo "<br>";
+        echo ("parentesis");
+        //Variables:
+        //String:
+        
+        $name = "Daniel";
+        $surname = 'Navarro';
+        echo "<br>";
+        echo $name;
+        echo "<br>";
+        echo $name . " - " . $surname;
+        echo "<br>";
+        echo "$name - $surname";
+
+       //numeros
+        echo "<br>";
+        $age = 20;
+        $suma = 12;
+        echo "<p>Tengo $age años </p>";
+        echo "tengo " .  ($age + $suma) . " años";
+        var_dump($age);
+        $age= "asdkalsd";
+        var_dump($age);
+        $age = false;
+        var_dump($age);
+        
+
+        //CONSTANTES
+        define("IVA_GENERAL", 0.21);
+        const IVA_REDUCIDO = 0.08;
+        $precio = 20.3;
+        echo "<p>El precio de IVA es: " . $precio + $precio * IVA_GENERAL . "</p>";
+        echo "<p>El precio de IVA reducido  es: " . $precio + $precio  * IVA_REDUCIDO . "</p>";
+
+            ?>
+    </p>
+</body>
+
+</html>
---
+++ Dwes/PHP1/practicaPersona1.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $tinto = 12.2341234;
+    echo number_format($tinto,2). "€";
+    echo "<br>";
+    $age = 21;
+    
+    if($age >= 18){
+        echo "<p>Puedes entrar</p>";
+    } else {
+        echo "<p>No puedes entrar</p>";
+    }
+
+    $edad = 20;
+    $dinero = 15;
+
+    if($edad >= 18 && $dinero>=10){
+        echo "Puedes entrar y tienes dinero";
+    } else {
+        echo "no puedes entrarxd";
+    }
+
+    $nombre = "Daniel";
+    $edad = 20;
+    $dinero = 50;
+    $esMiembro = true;
+
+    $producto = "Teclado";
+    $precio = 45;
+
+    /*if ($edad >= 20){
+        if($dinero > $precio){
+            if($esMiembro){
+                $descuento = $precio * 0.10;
+                $nuevoPrecio = $precio - $descuento;
+                echo "<br>";
+                echo "Precio original: " . $precio . "€";
+                echo "<br>";
+                echo "Descuento: " . $descuento;
+                 echo "<br>";
+                echo "Precio final:" . number_format($nuevoPrecio, 2) . "€";
+            } else {
+                echo "Precio final: " . $precio;
+            }
+        } else {
+            echo "<br>";
+            echo $nombre . " " . "no tienes suficiente dinero";
+        }
+    } else {
+        echo "<br>";
+        echo $nombre . " " . "no puedes realizar esta compra";
+    } */
+
+    if($edad < 18){
+        echo "Eres menor";
+    }
+    elseif($dinero < $precio){
+        echo "No tienes dinero";
+    }
+    elseif($esMiembro){
+        $descuento = $precio * 0.10;
+                $nuevoPrecio = $precio - $descuento;
+                echo "<br>";
+                echo "Precio original: " . $precio . "€";
+                echo "<br>";
+                echo "Descuento: " . $descuento;
+                echo "<br>";
+                echo "Precio final:" . number_format($nuevoPrecio, 2) . "€";
+            } else {
+                echo "Precio final: " . $precio;
+            }
+
+    $frutas = ["manzana", "platano", "fresa", "naranja"];
+    echo "<br>";echo "<br>";
+    echo $frutas[0];
+
+    foreach ($frutas as $fruta){
+    echo "<br>";   
+    echo $fruta;
+    }
+    echo "hola"
+    
+
+?>
+</body>
+</html>
