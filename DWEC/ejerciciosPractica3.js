53ed76e9d7a8e71fcf5cfb3dd316fc194161f434
Daniihzz <danyynavarro2812@gmail.com>
2026-09-17 11:09:47 +0200

    update 2026-09-17 11:09:47

---
+++ DWEC/ejerciciosPractica3.js
@@ -0,0 +1,8 @@
+console.log("........................")
+let total = prompt("Dame un numero")
+if(total % 2 == 0){
+    console.log("Es par")
+} else {
+    console.log ("Es impar")
+}
+console.log("........................")
