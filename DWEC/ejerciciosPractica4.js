51ca0cc06718f656d4f4ee5eceab87361c3fa511
Daniihzz <danyynavarro2812@gmail.com>
2026-09-17 12:22:25 +0200

    update 2026-09-17 12:22:25

--- DWEC/ejerciciosPractica4.js
+++ DWEC/ejerciciosPractica4.js
@@ -2,7 +2,7 @@ let num = Number(prompt("Dame un numero"))
 let total = []
 
 for(let i = 0; i<=10; i++){
-    total.push(num*i)
+    total.push([i, "x", num, "=", num*i])
 }
 
 console.table(total)
--- Dwes/PHP1/buclesYcondicionales.php
+++ Dwes/PHP1/buclesYcondicionales.php
@@ -15,8 +15,17 @@
         echo "<p>Eres menor de edad</p>";
     }
 
-    //Ternario
-    $mensaje = ""
+    $dia = 3;
+    $nombre = match(dia){
+        1 => "Lunes",
+        2 => "Martes",
+        3 => "Miercoles",
+        4 => "Jueves",
+        5 => "Viernes",
+        6,7=> "fin de semana",
+        default => "Dia no valido"
+    };
+    echo $nombre;
     ?>
 </body>
 </html>
