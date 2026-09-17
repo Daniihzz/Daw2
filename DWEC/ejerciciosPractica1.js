9ed5dc6d7a5ffa4e8837222655f16c071ba7e69d
Daniihzz <danyynavarro2812@gmail.com>
2026-09-17 10:02:58 +0200

    update 2026-09-17 10:02:58

---
+++ DWEC/ejerciciosPractica1.js
@@ -0,0 +1,54 @@
+let salir = false
+while (salir === false)  {
+console.log("-------------------------------")
+console.log("Pulse 1 para sumar")
+console.log("Pulse 2 para restar")
+console.log("Pulse 3 para multiplicar")
+console.log("Pulse 4 para dividir")
+console.log("Pulse 5 para obtener el modulo")
+console.log("Pulse 6 para salir")
+console.log("-------------------------------")
+let opcion = prompt("Selecciona una opcion")
+let total
+let numeritoA
+let numeritoB
+
+switch(opcion){
+    case "1":
+        numeritoA = prompt("Selecciona un numero")
+        numeritoB = prompt("Selecciona otro numero")
+        total = numeritoA + numeritoB
+        console.log(total)
+        break;
+        
+    case "2":
+        numeritoA = prompt("Selecciona un numero")
+        numeritoB = prompt("Selecciona otro numero")
+        total = numeritoA - numeritoB
+        console.log(total)        
+        break;
+    case "3":
+        numeritoA = Number(prompt("Selecciona un numero"))
+        numeritoB = Number(prompt("Selecciona un numero"))
+        total = numeritoA * numeritoB
+        console.log(total)
+        break;
+        
+    case "4":
+        numeritoA = Number(prompt("Selecciona un numero"))
+        numeritoB = Number(prompt("Selecciona un numero"))
+        total = numeritoA / numeritoB
+        console.log(total)
+        break;
+        
+    case "5":
+        numeritoA = Number(prompt("Selecciona un numero"))
+        numeritoB = Number(prompt("Selecciona un numero"))
+        total = numeritoA % numeritoB
+        console.log(total)
+        break;
+    case "6":
+        salir = confirm("¿Seguro que quieres salir?")
+        break;
+}
+}
