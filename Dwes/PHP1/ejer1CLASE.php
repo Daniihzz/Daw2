c02c7e4a5635153542e39b1471a83aada4ad774e
Daniihzz <danyynavarro2812@gmail.com>
2026-09-17 09:34:31 +0200

    update 2026-09-17 09:34:31

--- Dwes/PHP1/ejer1CLASE.php
+++ Dwes/PHP1/ejer1CLASE.php
@@ -33,36 +33,56 @@
         echo "<br>";
         echo "$name - $surname";
 
-       //numeros
+        //numeros
         echo "<br>";
         $age = 20;
         $suma = 12;
         echo "<p>Tengo $age años </p>";
-        echo "tengo " .  ($age + $suma) . " años";
+        echo "tengo " . ($age + $suma) . " años";
         var_dump($age);
-        $age= "asdkalsd";
+        $age = "asdkalsd";
         var_dump($age);
         $age = false;
-        var_dump($age); 
-        
+        var_dump($age);
+
 
         //CONSTANTES
         define("IVA_GENERAL", 0.21);
         const IVA_REDUCIDO = 0.08;
         $precio = 20.3;
-        echo "<p>El precio de IVA es: " . $precio + $precio * IVA_GENERAL . "</p>";
-        echo "<p>El precio de IVA reducido  es: " . $precio + $precio  * IVA_REDUCIDO . "</p>";
+        echo "<p>El precio de IVA es: $precio";
+
+        echo "<p>El precio de IVA reducido  es: " . $precio + $precio * IVA_REDUCIDO . "</p>";
 
         var_dump(PHP_VERSION);
 
-        var_dump (__LINE__);
+        var_dump(__LINE__);
         echo "<br>";
         var_dump(__FILE__);
+
+        $a = 5;
+        $b = $a ** 3;
+        var_dump($b);
+
+        $a = 7;
+        $mod = $a % 2;
+        var_dump($mod);
+
+        $a = 11;
+        $mod = $a % 4;
+        var_dump($mod);
+
+
+        $a = 1;
+        $a += 1;
+        var_dump($a);
+
         
-        echo __
+        $b = 10;
+        echo "<br>";
 
-
-            ?>
+        
+        ?>
     </p>
 </body>
 
