55f7277529a1ad79c6508aaf45f1719c4e7b0371
Daniihzz <danyynavarro2812@gmail.com>
2026-09-17 11:55:37 +0200

    update 2026-09-17 11:55:37

---
+++ DWEC/ejerciciosPractica.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $ejercicio = 1;
+    switch ($ejercicio){
+        case 1:
+            echo '<script src="ejerciciosPractica1.js"></script> ';
+            break;
+
+        case 2:
+            echo '<script src="ejerciciosPractica2.js"></script>';
+            break;
+        case 3:
+            echo '<script src="ejerciciosPractica3.js"></script>';
+            break;
+        case 4:
+            echo '<script src="ejerciciosPractica4.js"></script>';
+            break;
+            }
+       
+    ?>
+    </body>
+</html>
