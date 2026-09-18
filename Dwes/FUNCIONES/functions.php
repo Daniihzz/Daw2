b39e06d1299d4fff54d5ea6663f5633758312f50
Daniihzz <danyynavarro2812@gmail.com>
2026-09-18 14:12:21 +0200

    update 2026-09-18 14:12:21

--- Dwes/FUNCIONES/functions.php
+++ Dwes/FUNCIONES/functions.php
@@ -7,7 +7,23 @@
 </head>
 <body>
     <?php
-    
+    // Funcion que reciba una cantidad de 
+    // notas y devuelve la cantidad de personas aprobadas
+    function aprobados($notas):int{
+        $pasa = 0;
+        $noPasa = 0;
+        foreach($notas as $n){
+            if($n < 5){
+                $noPasa++;
+            } else {
+                $pasa++;
+            }
+        }
+        return $pasa;
+    }
+
+    $dani = [1,4,7,8,5,6,8,9,3];
+    echo aprobados($dani);
     ?>
 </body>
 </html>
