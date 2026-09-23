0773eab265d53248fa5505fed48c3ca6eb034dd6
Daniihzz <danyynavarro2812@gmail.com>
2026-09-23 10:10:46 +0200

    update 2026-09-23 10:10:46

---
+++ DWEC/PracticaPropia/Pract.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $respuesta = 1;
+    switch ($respuesta){
+        case 1:
+            echo '<script src="Pract1.js"></script>';
+            break;
+        case 2:
+
+            break;
+        case 3:
+
+            break;
+    }
+        
+    ?>
+</body>
+</html>
