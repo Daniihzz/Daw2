251fa7caf6b795040ca561d03884f4e1ba8e8231
Daniihzz <danyynavarro2812@gmail.com>
2026-09-23 09:08:01 +0200

    update 2026-09-23 09:08:01

---
+++ DWEC/EjerciciosVariables/ConverSion.js
@@ -0,0 +1,22 @@
+// EJERCICIO 3
+
+let edad = 20;
+let precio = 12.2;
+const PI = 3.14159;
+let nombre = "Daniel";
+let apellidos = "Navarro Hernandez";
+let codigoPostal = "28393";
+let MayorEdad = true;
+const DIAS_SEMANA = 7;
+let resultado;
+let respuesta = null; 
+let temperatura = -2.3;
+let mensaje = " ";
+let cantidad = "100";
+let activo = false;
+var contador = 0;
+
+
+// EJERCICIO 6
+
+let cade = "25";
---
+++ DWEC/EjerciciosVariables/ConverSion.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $respuesta = 1;
+    switch ($respuesta) {
+        case 1:
+            echo '<script src="ConverSion.js"></script>';
+            break;
+        case 2:
+
+            break;
+        case 3:
+
+            break;
+    }
+
+
+    ?>
+</body>
+
+</html>
